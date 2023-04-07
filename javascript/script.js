fe53f6aa74c67958d9ac539d5e51820d83b3c38d
KOTYAN4K <115372552+KOTYAN4K@users.noreply.github.com>
2023-04-07 10:21:52 +0300

    Добавил первый паралакс скрипт
    Пока что для карты сделал небольшой параллакс скрипт

--- javascript/script.js
+++ javascript/script.js
@@ -234,3 +234,20 @@ for(let info of infos){
         }
     });
 }
+
+
+
+//паралакс
+
+function parallax(){
+  var tie = document.getElementById('tie');
+  var deathstar = document.getElementById('deathstar');
+  
+
+  tie.style.top = (window.pageYOffset / 3) + 'px';
+  tie.style.left = -(window.pageYOffset / 2) + 'px';
+  deathstar.style.top = ( 1000 - window.pageYOffset) + 'px';
+  //deathstar.style.left = (-1700 + window.pageYOffset) + 'px';
+}
+
+window.addEventListener("scroll", parallax, false);
